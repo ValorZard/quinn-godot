621c4293a6813b0182430e883385015aab612142
Srayan Jana <valorzard.kiko@outlook.com>
2025-06-19 22:20:08 -0700

    able to sync positions, but why is there an extra sprite?

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -1,12 +1,12 @@
 use game_core::client::run_client;
 use game_core::server::run_server;
-use godot::classes::Sprite2D;
+use godot::classes::{Input, Sprite2D};
 use godot::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=Sprite2D)]
 pub struct Player {
-    speed: f64,
+    speed: f32,
     angular_speed: f64,
     pub player_id: game_core::PlayerId,
     #[export]
@@ -35,7 +35,7 @@ impl ISprite2D for Player {
 
     fn ready(&mut self) {}
 
-    fn physics_process(&mut self, delta: f64) {
+    fn physics_process(&mut self, delta: f32) {
         // GDScript code:
         //
         // rotation += angular_speed * delta
@@ -43,12 +43,30 @@ impl ISprite2D for Player {
         // position += velocity * delta
 
         if self.is_local {
-            let radians = (self.angular_speed * delta) as f32;
-            self.base_mut().rotate(radians);
+            let mut velocity = Vector2::new(0.0, 0.0);
 
-            let rotation = self.base().get_rotation();
-            let velocity = Vector2::UP.rotated(rotation) * self.speed as f32;
-            self.base_mut().translate(velocity * delta as f32);
+            // Note: exact=false by default, in Rust we have to provide it explicitly
+            let input = Input::singleton();
+            if input.is_action_pressed("move_right") {
+                velocity += Vector2::RIGHT;
+            }
+            if input.is_action_pressed("move_left") {
+                velocity += Vector2::LEFT;
+            }
+            if input.is_action_pressed("move_down") {
+                velocity += Vector2::DOWN;
+            }
+            if input.is_action_pressed("move_up") {
+                velocity += Vector2::UP;
+            }
+
+            if velocity.length() > 0.0 {
+                velocity = velocity.normalized() * self.speed;
+            }
+
+            let change = velocity * delta;
+            let position = self.base().get_global_position() + change;
+            self.base_mut().set_global_position(position);
         }
 
         // or verbose:
@@ -67,7 +85,7 @@ impl Player {
     }
 
     #[func]
-    fn increase_speed(&mut self, amount: f64) {
+    fn increase_speed(&mut self, amount: f32) {
         self.speed += amount;
         self.base_mut().emit_signal("speed_increased", &[]);
     }
